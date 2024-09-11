d8b8844d85c321370a11a1afba2d34bba147749e
smo <aleksandr.smolkov@sprecher-automation.com>
2024-09-11 11:08:41 +0200

    chore: update version of clai to 0.2.0
    - Bumped the version of the `clai` package from 0.1.8 to 0.2.0 in Cargo.toml and Cargo.lock.
    - Adjusted dependencies accordingly.
    - Cleaned up commented-out code in commit.rs that was not needed for the commit message generation.

--- src/command/commit.rs
+++ src/command/commit.rs
@@ -13,12 +13,10 @@ impl Commit {
         let git_diff = String::from_utf8_lossy(&output.stdout);
         let chat = Chat::new(client);
         let message = format!("Write a Git commit message for these changes: {}", git_diff);
-        // println!("{}", message);
         let message = Message::new("user", &message);
-        // let message1 = Message::new("user", "Write a Git commit message for following changes:");
-        // let message2 = Message::new("user", &git_diff);
         let response = chat.send(vec![message]).await?;
         println!("{}", response.choices.last().unwrap().message.content);
+		
         Ok(())
     }
 }
